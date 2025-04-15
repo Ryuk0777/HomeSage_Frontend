7adf6e02b09b0780ba1d9cbfb5ed6aebf5a0f7ba
Ryuk0777 <114020461+Ryuk0777@users.noreply.github.com>
2025-04-15 18:00:21 +0530

    Change loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Loader = () => {
   return (
-    <div className={`h-full w-full bg-slate-200 flex justify-center items-center`}>
-         <div className="w-16 h-16 border-4 border-cyan-500 border-t-transparent rounded-full animate-spin"></div>
+    <div className="h-full w-full bg-slate-200 flex justify-center items-center">
+      <div className="flex space-x-4">
+        <div className="w-4 h-4 bg-black rounded-full animate-bounce [animation-delay:0s]"></div>
+        <div className="w-4 h-4 bg-black rounded-full animate-bounce [animation-delay:0.2s]"></div>
+        <div className="w-4 h-4 bg-black rounded-full animate-bounce [animation-delay:0.4s]"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
